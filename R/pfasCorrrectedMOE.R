fab1bce7ec7ea2248e82378a5737bc8ea24a50ad
Judson <Judson.Richard@epa.gov>
2023-08-04 10:08:19 -0600

    delete unused code

--- R/pfasCorrrectedMOE.R
+++ R/pfasCorrrectedMOE.R
@@ -72,28 +72,15 @@ pfasCorrrectedMOE <- function() {
 
   res = mat[is.element(mat$dtxsid,dlist),]
   res = subset(res,select= -c(value_original,units_original,acname,desc))
-  # res$invitro.water.fraction = NA
-  # res$invitro.cell.fraction = NA
-  # res$invitro.vwater = NA
-  # res$invitro.vcell = NA
-  # res$invitro.vwell = NA
-  # res$Kmax = NA
   res$qc = NA
   res$mw = NA
   res$fub = NA
   res$blood2plasma = NA
-  # res$tissue2plasma = NA
   res$pod_min_uM = NA
   res$pod_min_source = NA
   res$pod_min_ngmL = NA
   res$plasma_conc_ngmL = NA
-  # res$tissue_pod_ngmL = NA
-  # res$tissue_conc_ngmL = NA
-  # res$conc_ratio = NA
-  # res$ac50_ratio = NA
-  # res$moe_ratio = NA
   res$moe = NA
-  # res$raw_moe = NA
 
   for(i in 1:nrow(res)) {
     dtxsid = res[i,"dtxsid"]
@@ -102,13 +89,6 @@ pfasCorrrectedMOE <- function() {
       if(length(dqc)>1) res[i,"qc"] = "P/F"
       else res[i,"qc"] = dqc
     }
-
-    # res[i,"invitro.cell.fraction"] = part[dtxsid,"invitro.cell.fraction"]
-    # res[i,"invitro.water.fraction"] = part[dtxsid,"invitro.water.fraction"]
-    # res[i,"invitro.vcell"] = part[dtxsid,"invitro.vcell"]
-    # res[i,"invitro.vwater"] = part[dtxsid,"invitro.vwater"]
-    # res[i,"invitro.vwell"] = part[dtxsid,"invitro.vwell"]
-    # res[i,"Kmax"] = part[dtxsid,"Kmax"]
     res[i,"fub"] = part[dtxsid,"fub"]
     res[i,"blood2plasma"] = part[dtxsid,"blood2plasma"]
 
@@ -123,24 +103,7 @@ pfasCorrrectedMOE <- function() {
     res[i,"pod_min_source"] = temp[1,"source"]
 
     res[i,"pod_min_ngmL"] = min(temp$pod) * mw
-    # res[i,"tissue_conc_ngmL"] = res[i,"plasma_conc_ngmL"] * res[i,"Kmax"] * res[i,"fub"]
-    # # res[i,"tissue_pod_ngmL"] =  res[i,"pod_min_ngmL"] *
-    #                             res[i,"invitro.cell.fraction"] *
-    #                             res[i,"invitro.vwell"] /
-    #                             res[i,"invitro.vcell"]
-
-    # res[i,"tissue_pod_ngmL"] =  res[i,"pod_min_ngmL"] *
-    #   res[i,"invitro.water.fraction"] *
-    #   res[i,"invitro.vwell"] /
-    #   res[i,"invitro.vwater"]
-    #
-    # res[i,"tissue2plasma"] = res[i,"tissue_conc_ngmL"]/res[i,"plasma_conc_ngmL"]
-    # res[i,"moe"] = res[i,"tissue_pod_ngmL"] / res[i,"tissue_conc_ngmL"]
     res[i,"moe"] = res[i,"pod_min_ngmL"] / res[i,"plasma_conc_ngmL"]
-
-    # res[i,"conc_ratio"] = res[i,"tissue_conc_ngmL"]/res[i,"plasma_conc_ngmL"]
-    # res[i,"ac50_ratio"] = res[i,"tissue_pod_ngmL"]/res[i,"pod_min_ngmL"]
-    # res[i,"moe_ratio"] =  res[i,"ac50_ratio"]/res[i,"conc_ratio"]
   }
 
   hist(log10(res$moe))
@@ -161,9 +124,4 @@ pfasCorrrectedMOE <- function() {
   }
   file = "data/PFAS min moe x assay.xlsx"
   write.xlsx(minpod,file)
-
-  # nlist = c("dtxsid","casrn","name","nickname","conc_ratio","ac50_ratio")
-  # temp = unique(res[,nlist])
-  # file = "data/PFAS PK conc ratios.xlsx"
-  # write.xlsx(temp,file)
 }
